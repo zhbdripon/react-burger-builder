93611870d15ce22f78db116f73bd4574106cef76
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-03-28 17:56:00 +0600

    added burgerbuilder component and added style to layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component{
 
@@ -7,7 +8,7 @@ class App extends Component{
     return (
       <div>
         <Layout>
-          <h1>Initialize Burger Builder</h1>
+          <BurgerBuilder/>
         </Layout>
       </div>
     );
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import Aux from '../../hoc/Aux'
+class BurgerBuilder extends Component{
+    render(){
+        return (
+            <Aux>
+                <div>Burger GUI</div>
+                <div>Burger control pannel</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder;
