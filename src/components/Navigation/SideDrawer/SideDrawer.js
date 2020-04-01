0a09aa93b9e412a83bd52e5e07dfeeae64cb797f
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-04-01 22:03:04 +0600

    sidedrawer hide on backdrop click

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import Logo from '../../Logo/Logo'
-import NavigationItems from '../NavigationItems/NavigationItems'
-import classes from './SideDrawer.module.css'
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
-const sideDrawer = () =>{
+const sideDrawer = (props) =>{
+
+    let sideDrawerClasses = [classes.SideDrawer];
+    sideDrawerClasses.push(props.open?classes.Open:classes.Close);
+    sideDrawerClasses = sideDrawerClasses.join(' ');
 
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}></Backdrop>
+            <div className={sideDrawerClasses}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        </Aux>
     )
 }
 
