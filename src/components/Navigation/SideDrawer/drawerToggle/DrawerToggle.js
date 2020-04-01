a6180ce70627f4d6bd9045427101d13c307573ba
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-04-02 00:02:44 +0600

    styling drawer toggle button

--- src/components/Navigation/SideDrawer/drawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/drawerToggle/DrawerToggle.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import classes from './DrawerToggler.module.css'
 
 const drawerToggle = (props) =>{
     return (
-        <div onClick={props.clicked}>...</div>
+        <div onClick={props.clicked} className={classes.DrawerToggle}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     )
 }
 
