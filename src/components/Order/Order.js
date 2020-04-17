f503b41a73f188ec1ec4cddf5b023d6dd640513b
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-04-18 02:13:28 +0600

    added Orders cockpit component and Order component, also dummy route on App.js

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Order.module.css'
+const order = (props) =>{
+    return (
+        <div className={classes.order}>
+            <p>Ingredients: </p>
+            <p>Price: <strong>USD 5.75</strong></p>
+        </div>
+    )
+}
+
+export default order;
