0795719865705411f970d497c4bdeb1e89c63a67
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-04-18 02:46:56 +0600

    corrected className in Order component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Order.module.css'
 const order = (props) =>{
     return (
-        <div className={classes.order}>
+        <div className={classes.Order}>
             <p>Ingredients: </p>
             <p>Price: <strong>USD 5.75</strong></p>
         </div>
