36548ef62e50955fbc531b58cd54cbc3dceab22d
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-04-24 20:38:36 +0600

    input elements based on state object

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,13 +7,59 @@ import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
-         name:'',
-         email:'',
-         address:{
-             street:'',
-             postalCode: '',
-         },
-         loading:false
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' },
+                    ]
+                },
+                value: ''
+            }
+        },
+        loading:false
     }
 
     orderHandler = (event) =>{
@@ -23,16 +69,6 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: +this.props.price.toFixed(2),
-            customer:{
-                name: 'Md. Ziaul Hoque',
-                address:{
-                    street: 'Teststreet 1',
-                    zipCode: '41351',
-                    country: 'Bangladesh'
-                },
-                email:'someone@example.com'
-            },
-            deliveryMethod: 'fastest'
         }
 
         axios.post('/orders.json',order)
@@ -52,12 +88,27 @@ class ContactData extends Component {
 
     render(){
 
+        const formElementArray = [];
+        for(let key in this.state.orderForm){
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
+        console.log(formElementArray.length)
+
         let form = (
             <form action="">
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-                <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+                {formElementArray.map(element=>(
+                        <Input
+                            key={element.id} 
+                            elementType={element.config.elementType}
+                            elementConfig={element.config.elementConfig}
+                            value={element.config.value}
+                        />
+                    )
+                )}
                 <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
             </form>
         )
