10a82be90e8383536a7e510260adf1dbe885779e
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-05-04 23:11:47 +0600

    fix: redirect to homepage from checkout

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import ContactData from './ContactData/ContactData'
 import { connect } from 'react-redux'
 
@@ -16,19 +16,23 @@ class Checkout extends Component{
     }
 
     render(){
-        return (
-            <div>
-                <CheckoutSummary 
-                    ingredients={this.props.ingds}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}
-                /> 
-                <Route 
-                    path={this.props.match.path+'/contact-data'} 
-                    component={ContactData}
-                />
-            </div>
-        )
+        let summary = <Redirect to="/"/>
+        if(this.props.ingds){
+            summary = (
+                <div>
+                    <CheckoutSummary 
+                        ingredients={this.props.ingds}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}
+                    /> 
+                    <Route 
+                        path={this.props.match.path+'/contact-data'} 
+                        component={ContactData}
+                    />
+                </div>
+            )
+        }
+        return summary;
     }
 }
 
