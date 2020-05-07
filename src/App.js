b8d756af2263f23d57e237ba08962ccde57e5115
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-05-07 23:34:19 +0600

    adding a logout link with component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
+
 import {Route,Switch} from 'react-router-dom'
 
 class App extends Component{
@@ -16,6 +18,7 @@ class App extends Component{
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder}/>
           </Switch>
         </Layout>
