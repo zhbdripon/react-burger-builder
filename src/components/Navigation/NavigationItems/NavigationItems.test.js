8c0ee78774b9abf3a365d5e5fd5fc72ae0862e4a
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-05-10 04:33:52 +0600

    unit test: added test for Nav items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {configure,shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({adapter: new Adapter()})
+
+describe('<NavigationItems/>',()=>{
+    it('should render two <NavigationItems/> elements if not authenticated',()=>{
+        const wrapper = shallow(<NavigationItems />);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+    it('should render three <NavigationItems/> elements if authenticated', () => {
+        const wrapper = shallow(<NavigationItems isAuthenticated />);
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+})
