984891794cdcaf5e0e8ee8c4268a7a9541a4d22f
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-05-10 04:40:08 +0600

    unit test: before each

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,12 +9,15 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()})
 
 describe('<NavigationItems/>',()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = shallow(<NavigationItems />);
+    })
     it('should render two <NavigationItems/> elements if not authenticated',()=>{
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
     it('should render three <NavigationItems/> elements if authenticated', () => {
-        const wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
