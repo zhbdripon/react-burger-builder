ab1bb06671ef80f82ad92f62be652e0ace61e16d
Md. Ziaul Hoque <zhbdripon@gmail.com>
2020-05-23 00:20:26 +0600

    UX: click burger logo to go back to home

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png'
 import classes from './Logo.module.css'
+
+import {NavLink} from 'react-router-dom';
+
 const logo = (props) => {
     return (
-        <div className={classes.Logo}>
-            <img src={burgerLogo} alt="MyBurger"/>
-        </div>
+        <NavLink
+            to="/"
+        >
+            <div className={classes.Logo}>
+                <img src={burgerLogo} alt="MyBurger"/>
+            </div>
+        </NavLink>
     )
 }
 
